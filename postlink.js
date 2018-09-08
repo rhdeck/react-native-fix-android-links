3b73dcbdf96726396b065883eadef7686d6ca066
Ray Deck <ray@raydeck.com>
2018-09-08 12:01:51 -0400

    Fixing what happens when you hit an already-fixed path

--- postlink.js
+++ postlink.js
@@ -10,19 +10,21 @@ const text = fs.readFileSync(sgpath, { encoding: "UTF8" });
 const lines = text.split("\n");
 //Now filter for projects
 const outlines = lines.map(line => {
-  if (line.trim().indexOf("project(") === 0) {
-    //Yeah let's check this one out
-    //split on single quotes to get the key arguments
-    const things = line.split("'");
-    const lib = things[1];
-    const path = things[3];
-    const startpath = Path.join(Path.dirname(sgpath), path);
-    //Lets correct the path to be absolute
-    const newpath = fs.realpathSync(startpath);
-    return "project('" + lib + "').projectDir = new File('" + newpath + "')";
-  } else {
+  if (line.trim().indexOf("project(") !== 0) {
     return line;
   }
+  //Yeah let's check this one out
+  //split on single quotes to get the key arguments
+  const things = line.split("'");
+  const lib = things[1];
+  const path = things[3];
+  if (!path.startsWith(".")) {
+    return line;
+  }
+  const startpath = Path.join(Path.dirname(sgpath), path);
+  //Lets correct the path to be absolute
+  const newpath = fs.realpathSync(startpath);
+  return "project('" + lib + "').projectDir = new File('" + newpath + "')";
 });
 const out = outlines.join("\n");
 fs.writeFileSync(sgpath, out);
